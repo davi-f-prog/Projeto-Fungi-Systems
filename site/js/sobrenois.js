050d8e55676ea3bf958e16112b52d882351062ec
Davi Freitas <davi.freitas@bandtec.com.br>
2020-04-17 22:48:48 -0300

    adicinando animação/pagina sobre

--- site/js/sobrenois.js
+++ site/js/sobrenois.js
@@ -5,7 +5,16 @@ function scroll() {
     navega.className = "navbar";
   } else {
     navega.className = "banana";
+  } if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+    itens.className = "animacao_icone";
+  } else {
+    itens.className = "icones";
+  }if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+    sobre_animacao.className = "anima_sobre_mim";
+  } else {
+    sobre_animacao.className = "sobre_mim";
   }
 }
+
 
 
