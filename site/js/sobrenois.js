8fb892258ab342e61879fa21ddb5bbcc871d0ac1
Davi Freitas <davi.freitas@bandtec.com.br>
2020-04-25 12:40:52 -0300

    Melhorando o design da pagina sobre

--- site/js/sobrenois.js
+++ site/js/sobrenois.js
@@ -4,17 +4,12 @@ function scroll() {
   if (document.body.scrollTop > 510 || document.documentElement.scrollTop > 510) {
     navega.className = "navbar";
   } else {
-    navega.className = "banana";
+    navega.className = "navscroll";
   } if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
-    itens.className = "animacao_icone";
+    itens.className = "animacao_icones";
   } else {
     itens.className = "icones";
-  }if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
-    sobre_animacao.className = "anima_sobre_mim";
-  } else {
-    sobre_animacao.className = "sobre_mim";
-  }
 }
-
+}
 
 
